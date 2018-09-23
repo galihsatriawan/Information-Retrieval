2f8c330e19e1cca769aecd9a78a52218e2d14559
galihsatriawan <satriawan.galih99@gmail.com>
2018-09-23 16:03:22 +0700

    Akhir Course 2: Metode TF-IDF

--- Similarity_Class.cpp
+++ Similarity_Class.cpp
@@ -7,17 +7,22 @@ class Dokumen{
     public :
         int no;
         string data; //kalimat utuh
+        int n;
         vector<string> kump_kata; //kumpulan kata
         map<string,int> kata_doc; //banyak kata per document
         set<string> detect_kata_doc; //kata unik tiap document
         vector<int> v;
+        vector<double> tf;
+        vector<double> tf_idf;
         Dokumen(int pos){
             no = pos;
         }
+
 };
 
 vector<Dokumen> docs; //seluruh data/document
 vector<string> kata_unik;
+map<string,double> df,idf;
 set<string> uniq; // Deteksi seluruh kata unik
 
 
@@ -47,6 +52,8 @@ vector<string> split_sent(Dokumen &doc, char delim){
         if(uniq.count(word)==0) {
             uniq.insert(word);
             kata_unik.push_back(word);
+            df.insert({word,0});
+            idf.insert({word,0});
         }
         //apakah unik bagi documennya
         if(doc.detect_kata_doc.count(word)==0){
@@ -77,6 +84,8 @@ vector<string> split_sent(Dokumen &doc, char delim){
         if(uniq.count(d)==0) {
             uniq.insert(d);
             kata_unik.push_back(d);
+            df.insert({d,0});
+            idf.insert({d,0});
         }
     //apakah unik bagi documennya 
         if(doc.detect_kata_doc.count(d)==0){
@@ -103,7 +112,7 @@ vector<string> add_data(string d){
     Dokumen doc(docs.size()+1); // No Dokumen 
     doc.data = d;
     doc.kump_kata = find_uniq(doc);
-    
+    doc.n = doc.kump_kata.size();
     //tambahkan ke kumpulan dokumen
     docs.push_back(doc);
     return doc.kump_kata;
@@ -112,6 +121,8 @@ void clear_all_vector(){
     for(int j=0;j<docs.size();j++){
             Dokumen *d = &(docs.at(j));
             d->v.clear();
+            d->tf.clear();
+            d->tf_idf.clear();
     }
 }
 void vectorize(){
@@ -119,14 +130,42 @@ void vectorize(){
     clear_all_vector();
     for(int i=0;i<kata_unik.size();i++){
         string word= kata_unik.at(i);
+        int cnt_df =0;
         for(int j=0;j<docs.size();j++){
 
             Dokumen *d = &(docs.at(j));
             bool ada = d->detect_kata_doc.count(word)>0;
+            //Jika suatu kata unik ada di dokumen
+            if(ada) cnt_df++;
+
             int cnt = ada ? d->kata_doc.at(word) : 0; 
             //cout <<word<<"__"<<cnt <<" ";
             d->v.push_back(cnt);
-            
+            d->tf.push_back(1.0*cnt/d->n);
+        }
+        //replace df
+        df.erase(word);
+        idf.erase(word);
+
+        df.insert({word,cnt_df});
+        double t_idf = log10(docs.size()/(1.0*cnt_df)); // 1.0 disini hanya untuk casting ke double
+        idf.insert({word,t_idf});
+
+    }
+}
+/*
+  1. Vectorize terlebih dahulu (untuk mendapatkan Vectorization awal dan tf,df,idf)
+  2. Count tf * idf
+*/
+#define For(i,a,n) for(int i=a;i<=n;i++)
+void count_tf_idf(){
+    for(int i=0;i<kata_unik.size();i++){
+        int j;
+        string word = kata_unik[i];
+        For(j,0,docs.size()-1){
+            Dokumen *d = &docs[j];
+            // Karena index kata unik sama dengan index di tf nya
+            d->tf_idf.push_back((d->tf[i]) * (idf[word])); // push back dimulai dari 0
         }
     }
 }
@@ -158,6 +197,36 @@ void print_all_vector(){
         cout <<"D"<<(i+1)<<":";print_vector(docs.at(i));
     }
 }
+void print_df_idf(string cmd){
+    map<string,double> mp;
+    if(cmd.compare("df")==0) mp.insert(df.begin(),df.end());
+    else mp.insert(idf.begin(),idf.end());
+    int banyak = kata_unik.size();
+    for(int i = 0;i<banyak;i++){
+        if(i==0) cout << "["<<mp[kata_unik[0]];
+            else if(i==banyak-1) cout <<","<< mp[kata_unik[banyak-1]]<<"]"<<endl; 
+            else cout<<","<<mp[kata_unik[i]];
+    } 
+}
+void print_tf_idf(Dokumen doc,string cmd){
+    vector<double> vt;
+    if(cmd.compare("tf_idf")==0)  vt= doc.tf_idf;
+    else vt = doc.tf;
+    int banyak = vt.size();
+    // cout<< banyak<<"<--"<<endl;
+    for(int i = 0;i<banyak;i++){   
+            if(i==0) cout << "["<<vt[0];
+            else if(i==banyak-1) cout <<","<< vt[banyak-1]<<"]"<<endl; 
+            else cout<<","<<vt[i];
+    }
+}
+void print_all_v_tf_idf(string cmd){
+
+    for(int i =0;i<docs.size();i++){
+        // cout << docs.at(i).kump_kata.size()<<"++"<<endl;;
+        cout <<"D"<<(i+1)<<":";print_tf_idf(docs.at(i),cmd);
+    }
+}
 void print_uniq(){
     int banyak =kata_unik.size();
     for(int i=0;i<banyak;i++){
@@ -177,19 +246,30 @@ void print_docs(){
     }
 }
 #define forVI(it,d) for(vector<int>::iterator it=d.begin();it!=d.end();it++)
-double scalar_doc(Dokumen d){
+#define forVDo(it,d) for(vector<double>::iterator it=d.begin();it!=d.end();it++)
+double scalar_doc(Dokumen d,string cmd){
     double temp=0;
     //penjumlahan seluruh kuadrat
-    forVI(it,d.v){
-        temp+= (*it)*(*it);
+    if(cmd.compare("tf_idf")==0){
+        forVDo(it,d.tf_idf){
+            temp+= (*it)*(*it);
+        }
     }
+    else{
+        forVI(it,d.v){
+            temp+= (*it)*(*it);
+        }
+    }
+    
     //cout << temp<<endl;
     return sqrt(temp);
 }
-double cross_vector(Dokumen a,Dokumen b){
+double cross_vector(Dokumen a,Dokumen b,string cmd){
+    
     double sum=0;
     for(int i=0;i<kata_unik.size();i++){
-        sum += (a.v[i]*b.v[i]);
+        if(cmd.compare("v_awal")==0)  sum += (a.v[i]*b.v[i]);
+        else sum += (a.tf_idf[i]*b.tf_idf[i]);
     }
     return sum;
 }
@@ -213,15 +293,31 @@ int main(){
   add_data("burung kakak tua");
   add_data("nenek sudah tua");
   add_data("burung sudah tua");
+  add_data("tua");
+  add_data("kakak nenek");
 
   print_docs();
   print_uniq();
   vectorize();
-  print_all_vector();
+  count_tf_idf();
+  //print_all_vector();
+  //print_all_v_tf_idf("tf");
+  print_df_idf("df");
+  print_df_idf("idf");
+  print_all_v_tf_idf("tf_idf");
+  
 //   cout << "Scalar D1 : "<<scalar_doc(docs[0])<<endl;
     // print_vector(docs.at(0));
-  double d1=scalar_doc(docs[0]),d3=scalar_doc(docs[2]);  
-  double similar13 = cross_vector(docs[0],docs[2])/(d1*d3);
+  /* 
+  double d1=scalar_doc(docs[0],"v_awal"),d3=scalar_doc(docs[2],"v_awal");  
+  double similar13 = cross_vector(docs[0],docs[2],"v_awal")/(d1*d3);
   cout <<"Similarity D1 & D3 : "<<  similar13 << endl;
+  */
+
+double d1=scalar_doc(docs[0],"tf_idf"),d4=scalar_doc(docs[3],"tf_idf");  
+  cout <<"|D1| = "<<d1<<" |D4| ="<<d4<<endl;
+  double similar14 = cross_vector(docs[0],docs[3],"tf_idf")/(d1*d4);
+  cout <<"Similarity D1 & D4 : "<<  similar14 << endl;
+  
  return 0;
 }
